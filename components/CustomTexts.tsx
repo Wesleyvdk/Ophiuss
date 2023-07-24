1a80b87485b752694f232d6143690e6e7185e18f
wesleyvdk <shinxisrl@outlook.com>
2023-07-24 14:17:57 +0200

    new home page

--- components/CustomTexts.tsx
+++ components/CustomTexts.tsx
@@ -1,4 +1,4 @@
-import { motion, Variants } from "framer-motion";
+import { motion } from "framer-motion";
 import { textContainer, textVariant2 } from "../utils/motion";
 import { ReactElement } from "react";
 
